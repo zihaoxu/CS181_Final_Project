c74d835d1559c23c1ab9278a35bd86b5acec2845
Zihao Xu <zxql2015@mymail.pomona.edu>
2017-11-26 10:15:39 -0800

    NLTK clf finalized

---
+++ 1. NLTK Classifier/sentiment_mod.py
@@ -0,0 +1,99 @@
+import nltk
+import random
+from nltk.classify.scikitlearn import SklearnClassifier
+import pickle
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from nltk.classify import ClassifierI
+from statistics import mode
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+
+
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+
+
+documents_f = open("pickled_algos/documents.pickle", "rb")
+documents = pickle.load(documents_f)
+documents_f.close()
+
+
+
+
+word_features5k_f = open("pickled_algos/word_features5k.pickle", "rb")
+word_features = pickle.load(word_features5k_f)
+word_features5k_f.close()
+
+
+ps = PorterStemmer()
+
+def find_features(document):
+    words = set(document.split())
+    features = {}
+    for w in words:
+        w = w.lower().replace('.', '').replace(',', '').replace('!', '')
+        w = ps.stem(w)
+        features[w] = (w in word_features)
+    return features
+
+
+open_file = open("pickled_algos/LogisticRegression_classifier5k.pickle", "rb")
+LogisticRegression_classifier = pickle.load(open_file)
+open_file.close()
+
+
+open_file = open("pickled_algos/LinearSVC_classifier5k.pickle", "rb")
+LinearSVC_classifier = pickle.load(open_file)
+open_file.close()
+
+
+open_file = open("pickled_algos/SGDC_classifier5k.pickle", "rb")
+SGDClassifier_classifier = pickle.load(open_file)
+open_file.close()
+
+
+
+
+voted_classifier = VoteClassifier(SGDClassifier_classifier,
+                                  LogisticRegression_classifier, 
+                                  LinearSVC_classifier) 
+
+
+
+
+def sentiment(text):
+    feats = find_features(text)
+    print(feats)
+    return voted_classifier.classify(feats),voted_classifier.confidence(feats)
+
+
+
+
+
+
+
+
+
+
+
